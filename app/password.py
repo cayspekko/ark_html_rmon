0b8311a2196e3b82d7268a5f909734983b6ffcaa
cayspekko <cayspekko@gmail.com>
2022-06-15 20:04:54 -0600

    add a script to change passwords

---
+++ app/password.py
@@ -0,0 +1,19 @@
+from passlib.context import CryptContext
+from tinydb import TinyDB, Query
+from tinydb.operations import set as db_set
+
+def change_password(username, psw):
+    User = Query()
+    users = TinyDB('db.json')
+
+    pwd_context = CryptContext(schemes=["bcrypt"], deprecated="auto")
+    new_pw = pwd_context.hash(psw)
+
+    users.upsert({'username': username, 'password': new_pw}, User.username == username)
+
+if __name__ == "__main__":
+    import sys
+    username = sys.argv[1]
+    password = sys.argv[2]
+
+    change_password(username, password)
